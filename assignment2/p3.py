6d713beac9e08046880c278d3c37661506fb776b
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2020-08-18 20:47:48 +0530

    fixed matrix formatting

--- assignment2/p3.py
+++ assignment2/p3.py
@@ -7,9 +7,20 @@ with open('mpy.txt', 'r') as f:
 with open('npy.txt', 'r') as f:
     n = [[int(num) for num in line.split(',')] for line in f]
 
-print("M =", m)
-print("N =", n)
-print("A =", a)
+# Print matrix M
+print("M = ")
+for i in range(3):
+    print(m[i])
+
+print("\n")
+# Print matrix N
+print("N = ")
+for j in range(3):
+    print(n[j])
+
+print("\n")
+# Print vector A
+print("A =", a, "\n")
 
 # calculating M x N
 prod = [[0,0,0], [0,0,0], [0,0,0]]
@@ -18,7 +29,9 @@ for i in range(3):
         for k in range(3):
             prod[i][j] += m[i][k]*n[k][j]
 
-print("M x N =", prod)  # displays M x N
+print("M x N =")  # displays M x N
+for k in range(3):
+    print(prod[k])
 
 # calculating M x A
 vec = [0, 0, 0]
@@ -26,13 +39,30 @@ for i in range(3):
     for k in range(3):
         vec[i] += m[i][k] * a[k]
 
+print("\n")
 print("M x A =", vec)
 
 
 # Files being read: mpy.txt, npy.txt
-# Output
-# M = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-# N = [[13, 0, 3], [8, 29, 17], [4, 37, 45]]
+# Output:
+# M =
+# [1, 2, 3]
+# [4, 5, 6]
+# [7, 8, 9]
+
+
+# N =
+# [13, 0, 3]
+# [8, 29, 17]
+# [4, 37, 45]
+
+
 # A = [1, 4, 9]
-# M x N = [[41, 169, 172], [116, 367, 367], [191, 565, 562]]
+
+# M x N =
+# [41, 169, 172]
+# [116, 367, 367]
+# [191, 565, 562]
+
+
 # M x A = [36, 78, 120]
