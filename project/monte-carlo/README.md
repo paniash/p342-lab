ee5a3d5919a020b35003713844c0ee7252d89f3b
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2021-01-29 00:41:38 +0530

    added finished to ellisoid problem

--- project/monte-carlo/README.md
+++ project/monte-carlo/README.md
@@ -1,3 +1,13 @@
 # Estimation of the volume of an ellisoid using Monte-Carlo
 
 `ellipsoid.py` contains helper functions for estimating the volume of the ellisoid using Monte-Carlo (`estimate.py`) and generating the resultant 3D plot (`volume3d.py`)
+
+## Plots
+
+### Estimate of volume
+
+![Estimate](./estimate.png)
+
+### 3D Plot of ellipsoid
+
+![Volume](./volume3d.png)
--- project/monte-carlo/estimate.py
+++ project/monte-carlo/estimate.py
@@ -1,6 +1,6 @@
 # Imports
 from ellipsoid import *
-
+import numpy as np
 
 # Define semi-axes
 a = 1.0
@@ -12,10 +12,13 @@ N_max = 50000
 steps = [ i for i in range(500, N_max, 100) ]
 
 y, x = list_volumes(steps, a, b, c)
-plt.plot(x, y, label='estimate')
+plt.plot(x, y, label='Estimated volume')
 
 exact_x = np.linspace(0, 50000, 100)
 exact_y = np.ones(100) * 12.566
-plt.plot(exact_x, exact_y, label='exact')
+plt.plot(exact_x, exact_y, label='Exact volume = 12.566 units$^3$')
+plt.xlabel('Number of steps $N$')
+plt.ylabel('Estimated volume of ellisoid $V$')
+plt.title('Estimation of the volume of ellipsoid using Monte-Carlo')
 plt.legend()
 plt.show()
