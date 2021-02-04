7cf19bb729fc6aaf193fbe58dbfc5eb0ac7b5a12
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2021-02-04 12:58:59 +0530

    fixed commenting

--- endsem/q4.py
+++ endsem/q4.py
@@ -15,13 +15,13 @@ def d2ydt2(t, y, z):
 def dydt(t, y, z):
     return z
 
-# Define boundaries
+# Define boundary values
 t_initial = 0
 t_final = 5
 y_initial = 2
 y_final = 45
 
-# Guesses taken for dy/dx is 10 and 100
+# Guesses taken for dy/dx are 10 and 100
 t, y, z = shooting_method(d2ydt2, dydt, t_initial, y_initial, t_final, y_final, 10, 100, step_size=0.05)
 
 # Print launch velocity dy/dt at t=0 => z(t=0) = z[0]
